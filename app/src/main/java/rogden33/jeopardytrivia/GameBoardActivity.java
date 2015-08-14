7d55d924318194e63559d2c5b1ca13f929ddd301
Robert <tt47cf6@users.noreply.github.com>
2015-08-13 21:20:07 -0700

    added board fetching

--- app/src/main/java/rogden33/jeopardytrivia/GameBoardActivity.java
+++ app/src/main/java/rogden33/jeopardytrivia/GameBoardActivity.java
@@ -1,5 +1,6 @@
 package rogden33.jeopardytrivia;
 
+import android.content.res.Configuration;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,6 +9,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import rogden33.jeopardytrivia.model.Clue;
+import rogden33.jeopardytrivia.model.GameBoard;
 
 
 public class GameBoardActivity extends ActionBarActivity {
@@ -16,30 +21,48 @@ public class GameBoardActivity extends ActionBarActivity {
 
     public static final int[] PRIZES = {100, 200, 400, 600, 800, 1000};
 
+    private String[] myCategories;
+
+    private Clue[][] myClues;
+
+    private boolean myDisplayFlag = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_board);
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-        // populate categories
-        String[] cats = new String[] {"cat 1", "cat 2", "cat 3", "cat 4", "cat 5"};
+        new GameBoard(this, PRIZES.length, NUM_OF_CATEGORIES).requestBoard();
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Toast.makeText(getApplicationContext(), "You are encouraged to switch to landscape orientation", Toast.LENGTH_LONG).show();
+        }
+        myClues = new Clue[PRIZES.length][NUM_OF_CATEGORIES];
+    }
+
+    public void display(GameBoard board) {
+        myCategories = board.getCategories();
+        for(int cat = 0; cat < NUM_OF_CATEGORIES; cat++) {
+            for (int row = 0; row < PRIZES.length; row++) {
+                myClues[row][cat] = board.getClue(row, cat);
+            }
+        }
+        myDisplayFlag = true;
+        populateBoard();
+    }
+
+    public void populateBoard() {
         for(int cat = 0; cat < NUM_OF_CATEGORIES; cat++) {
             int catID = getResources().getIdentifier("gameBoard_TextView_category" + cat, "id", getPackageName());
             TextView categoryDisplay = (TextView) findViewById(catID);
-            categoryDisplay.setText(cats[cat]);
+            categoryDisplay.setText(myCategories[cat]);
             for (int row = 0; row < PRIZES.length; row++) {
                 String name = String.format("gameBoard_Button_row%d_col%d", row, cat);
-                Log.i("NAME", name);
                 int id = getResources().getIdentifier(name, "id", getPackageName());
                 Button button = (Button) findViewById(id);
                 button.setText("$" + (PRIZES[row]));
             }
         }
-        // populate prize values
-
-
-
     }
 
 
--- app/src/main/java/rogden33/jeopardytrivia/model/GameBoard.java
+++ app/src/main/java/rogden33/jeopardytrivia/model/GameBoard.java
@@ -1,10 +1,25 @@
 package rogden33.jeopardytrivia.model;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
+
+import rogden33.jeopardytrivia.GameBoardActivity;
 
 public class GameBoard {
 
@@ -22,22 +37,21 @@ public class GameBoard {
 
     private Random myRandom;
 
-    public GameBoard(int rows, int cats) {
+    private GameBoardActivity myActivity;
+
+    private GameBoard mySelf = this;
+
+    public GameBoard(GameBoardActivity activity, int rows, int cats) {
         myNumberOfRows = rows;
         myNumberOfCategories = cats;
         myClues = new String[rows][cats];
         myCorrectResponses = new String[rows][cats];
         myRandom = new Random();
+        myActivity = activity;
+        myCategories = new String[myNumberOfCategories];
     }
 
-    public void setCategories(String[] cats) throws IllegalArgumentException {
-        if (cats.length != myNumberOfCategories) {
-            throw new IllegalArgumentException("Number of categories does not match!");
-        }
-        myCategories = cats.clone();
-    }
-
-    public void setClueResponse(int row, int cat, String clue, String response) throws IllegalArgumentException {
+    private void setClueResponse(int row, int cat, String clue, String response) throws IllegalArgumentException {
         if (row >= myNumberOfRows || cat >= myNumberOfCategories) {
             throw new IllegalArgumentException("Index out of bounds!");
         }
@@ -49,17 +63,28 @@ public class GameBoard {
         return myCategories.clone();
     }
 
-    public String getClue(int row, int cat) throws IllegalArgumentException {
+    public Clue getClue(int row, int cat) throws IllegalArgumentException {
         if (row >= myNumberOfRows || cat >= myNumberOfCategories) {
             throw new IllegalArgumentException("Index out of bounds!");
         }
-        return myClues[row][cat];
+        String clue = myClues[row][cat];
+        String[] possible = new String[SELECTABLE_RESPONSES];
+        int correctIndex = -1;
+        String category = myCategories[cat];
+        List<String> responses = getPossibleAnswers(row, cat);
+        String correct = responses.get(0);
+        Collections.shuffle(responses);
+        for (int i = 0; i < SELECTABLE_RESPONSES; i++) {
+            String resp = responses.get(i);
+            if (resp.equals(correct)) {
+                correctIndex = i;
+            }
+            possible[i] = responses.get(i);
+        }
+        return new Clue(clue, correct, category, "", 0, possible, correctIndex);
     }
 
-    public List<String> getPossibleAnswers(int row, int cat) throws IllegalArgumentException {
-        if (row >= myNumberOfRows || cat >= myNumberOfCategories) {
-            throw new IllegalArgumentException("Index out of bounds!");
-        }
+    private List<String> getPossibleAnswers(int row, int cat) {
         Set<String> possible = new HashSet<String>();
         while (possible.size() < SELECTABLE_RESPONSES - 1) {
             int randRow = myRandom.nextInt(myNumberOfRows);
@@ -70,5 +95,109 @@ public class GameBoard {
         List<String> result = new ArrayList<String>(possible);
         result.add(0, myCorrectResponses[row][cat]);
         return result;
+    }
+
+    public void requestBoard() {
+        new RandomCategoryGetter().execute();
+    }
+
+    private class RandomCategoryGetter extends AsyncTask<Void, Void,
+            Void> {
+
+        private List<String> myCategoryIDs;
+
+        @Override
+        protected Void doInBackground(Void... v) {
+            myCategoryIDs = new LinkedList<String>();
+            try {
+                int index = 0;
+                fetchMoreCategories();
+                while (index < myNumberOfCategories) {
+                    if (myCategoryIDs.isEmpty()) {
+                        fetchMoreCategories();
+                    }
+                    index = fillOneCategory(myCategoryIDs.remove(0), index);
+                }
+            } catch (Exception e) {
+
+            }
+            return null;
+        }
+
+        private void fetchMoreCategories() throws Exception {
+            Set<String> categories = new HashSet<String>(myCategoryIDs);
+            myCategoryIDs.clear();
+            URL url = new URL("http://jservice.io/api/random?count=50");
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                builder.append(line);
+            }
+            br.close();
+            // parse JSON object
+            JSONArray jsonarray = new JSONArray(builder.toString());
+            for (int i = 0; i < jsonarray.length(); i++) {
+                try {
+                    JSONObject jsonObject = (JSONObject) jsonarray.get(i);
+                    String category = jsonObject.getJSONObject("category").getString("id");
+                    categories.add(category);
+                } catch (JSONException e) {
+                    // that's ok, just skip
+                }
+            }
+            myCategoryIDs.addAll(categories);
+        }
+
+        private int fillOneCategory(String category, int index) throws Exception {
+            URL url = new URL("http://jservice.io/api/category?id=" + category);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000);
+            conn.setConnectTimeout(15000);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String line;
+            StringBuilder builder = new StringBuilder();
+            while ((line = br.readLine()) != null) {
+                builder.append(line);
+            }
+            br.close();
+            JSONObject object = new JSONObject(builder.toString());
+            JSONArray cluesArray = (object).getJSONArray("clues");
+            String categroyTitle = object.getString("title");
+            if (cluesArray.length() < myNumberOfRows) {
+                return index;
+            }
+            for (int i = 0; i < cluesArray.length(); i++) {
+                try {
+                    JSONObject jsonObject = (JSONObject) cluesArray.get(i);
+                    String response = jsonObject.getString("answer");
+                    String clue = jsonObject.getString("question");
+                    if (i < myNumberOfRows) {
+                        setClueResponse(i, index, clue, response);
+                    } else {
+                        i = cluesArray.length();
+                    }
+                } catch (JSONException e) {
+                    return index;
+                }
+            }
+            myCategories[index] = categroyTitle;
+            return index + 1;
+        }
+
+        @Override
+        protected void onPostExecute(Void v) {
+            super.onPostExecute(v);
+            myActivity.display(mySelf);
+        }
     }
 }
