bf57cf8153607618bf83d5299bc3d9f954ba87fa
Robert <tt47cf6@users.noreply.github.com>
2015-08-16 16:58:58 -0700

    added documentation

--- app/src/androidTest/java/rogden33/jeopardytrivia/junit/junit/GameBoardLocationTest.java
+++ app/src/androidTest/java/rogden33/jeopardytrivia/junit/junit/GameBoardLocationTest.java
@@ -5,43 +5,59 @@ import junit.framework.TestCase;
 import rogden33.jeopardytrivia.model.GameBoardLocation;
 
 /**
- * Created by Robert on 8/16/2015.
+ * Tests the GameBoardLocation object to ensure the equals and hashcode methods are behaving
+ * properly.
  */
 public class GameBoardLocationTest extends TestCase {
 
+    /**
+     * Test two objects which should equal each other, do.
+     */
     public void testEquals() {
         GameBoardLocation loc1 = new GameBoardLocation(1, 2);
         GameBoardLocation loc2 = new GameBoardLocation(1, 2);
-        assertTrue(loc1.equals(loc2));
-        assertTrue(loc2.equals(loc1));
+        assertTrue("Objects should have been equal to each other", loc1.equals(loc2));
+        assertTrue("Objects should have been equal to each other", loc2.equals(loc1));
     }
 
+    /**
+     * Test two object which should not equal each other, don't.
+     */
     public void testNotEquals() {
         GameBoardLocation loc1 = new GameBoardLocation(2, 2);
         GameBoardLocation loc2 = new GameBoardLocation(1, 2);
-        assertFalse(loc1.equals(loc2));
-        assertFalse(loc2.equals(loc1));
+        assertFalse("Objects should not have been equal to each other", loc1.equals(loc2));
+        assertFalse("Objects should not have been equal to each other", loc2.equals(loc1));
     }
 
+    /**
+     * Test two objects which should equal each other, do.
+     */
     public void testEqualsEdge() {
         GameBoardLocation loc1 = new GameBoardLocation(0, -1);
         GameBoardLocation loc2 = new GameBoardLocation(0, -1);
-        assertTrue(loc1.equals(loc2));
-        assertTrue(loc2.equals(loc1));
+        assertTrue("Objects should have been equal to each other", loc1.equals(loc2));
+        assertTrue("Objects should have been equal to each other", loc2.equals(loc1));
     }
 
+    /**
+     * Test two object which should not equal each other, don't.
+     */
     public void testNotEqualsEdge() {
         GameBoardLocation loc1 = new GameBoardLocation(0, 0);
         GameBoardLocation loc2 = new GameBoardLocation(-1, -1);
-        assertFalse(loc1.equals(loc2));
-        assertFalse(loc2.equals(loc1));
+        assertFalse("Objects should not have been equal to each other", loc1.equals(loc2));
+        assertFalse("Objects should not have been equal to each other", loc2.equals(loc1));
     }
 
+    /**
+     * Test that the hashCode and equals methods both behave in the same way.
+     */
     public void testHashCodeAndEquals() {
         GameBoardLocation loc1 = new GameBoardLocation(1, 2);
         GameBoardLocation loc2 = new GameBoardLocation(1, 2);
-        assertTrue(loc1.hashCode() == loc2.hashCode());
-        assertTrue(loc1.equals(loc2));
-        assertTrue(loc2.equals(loc1));
+        assertTrue("Hashcodes did not match and should have", loc1.hashCode() == loc2.hashCode());
+        assertTrue("Objects should have been equal to each other", loc1.equals(loc2));
+        assertTrue("Objects should have been equal to each other", loc2.equals(loc1));
     }
 }
--- app/src/androidTest/java/rogden33/jeopardytrivia/junit/junit/GameBoardTest.java
+++ app/src/androidTest/java/rogden33/jeopardytrivia/junit/junit/GameBoardTest.java
@@ -7,79 +7,117 @@ import rogden33.jeopardytrivia.model.Displayable;
 import rogden33.jeopardytrivia.model.GameBoard;
 
 /**
- * Created by Robert on 8/16/2015.
+ * Tests the GameBoard class.
  */
-public class GameBoardTest extends TestCase implements Displayable{
+public class GameBoardTest extends TestCase implements Displayable {
 
+    /**
+     * The number of categories to test with.
+     */
     private static final int NUM_OF_CATEGORIES = 5;
 
+    /**
+     * The number of rows to test with.
+     */
     private static final int NUM_OF_ROWS = 5;
 
+    /**
+     * A reference to the board.
+     */
     private GameBoard myBoard;
 
+    /**
+     * A flag to indicate when the board is loaded and ready.
+     */
     private boolean myReadyFlag;
 
+    /**
+     * Constructs a new board and blocks while it is loading.
+     */
     public void setUp() {
         myReadyFlag = false;
+        // request new board
         GameBoard board = new GameBoard(this, NUM_OF_ROWS, NUM_OF_CATEGORIES);
         board.requestBoard();
+        // spin until it is ready
         while (!myReadyFlag) {}
     }
 
+    /**
+     * Make sure the categories get assigned and the board can be successfully created.
+     */
     public void testCreation() {
         assertNotNull(myBoard.getCategories());
     }
 
+    /**
+     * Test that all categories are not null and are not empty.
+     */
     public void testCategories() {
         for (String cat : myBoard.getCategories()) {
-            assertNotNull(cat);
-            assertTrue(cat.length() > 0);
+            assertNotNull("Category should not be null", cat);
+            assertTrue("Category should not be an empty string", cat.length() > 0);
         }
     }
 
+    /**
+     * Test that there are no escaped characters in the final string outputs.
+     */
     public void testNoEscapes() {
         for (int cat = 0; cat < NUM_OF_CATEGORIES; cat++) {
             for (int row = 0; row < NUM_OF_ROWS; row++) {
                 Clue clue = myBoard.getClue(row, cat);
-                assertFalse(clue.getCategory().contains("\\"));
-                assertFalse(clue.getClue().contains("\\"));
-                assertFalse(clue.getResponse().contains("\\"));
+                assertFalse("An escaped character was found in the category", clue.getCategory().contains("\\"));
+                assertFalse("An escaped character was found in the clue", clue.getClue().contains("\\"));
+                assertFalse("An escaped character was found in the response", clue.getResponse().contains("\\"));
                 for (String resp : clue.getSelectableAnswers()) {
-                    assertFalse(resp.contains("\\"));
+                    assertFalse("An escaped character was found in a selectable response", resp.contains("\\"));
                 }
             }
         }
     }
 
+    /**
+     * Test that all the categories of all clues in a column match the category title.
+     */
     public void testCategoriesMatch() {
         for (int cat = 0; cat < NUM_OF_CATEGORIES; cat++) {
             String expected = myBoard.getCategories()[cat];
             for (int row = 0; row < NUM_OF_ROWS; row++) {
                 Clue clue = myBoard.getClue(row, cat);
-                assertEquals(true, expected.equals(clue.getCategory()));
+                assertEquals("Clue's category does not match the category title", true, expected.equals(clue.getCategory()));
             }
         }
     }
 
+    /**
+     * Ensure all fields of a clue are not null or empty strings.
+     */
     public void testForNonNull() {
         for (int cat = 0; cat < NUM_OF_CATEGORIES; cat++) {
             for (int row = 0; row < NUM_OF_ROWS; row++) {
                 Clue clue = myBoard.getClue(row, cat);
-                assertNotNull(clue);
-                assertNotNull(clue.getCategory());
-                assertNotNull(clue.getClue());
-                assertNotNull(clue.getResponse());
-                assertTrue(clue.getCorrectResponseIndex() >= 0);
-                assertTrue(clue.getCategory().length() > 0);
-                assertTrue(clue.getResponse().length() > 0);
+                assertNotNull("Clue is null", clue);
+                assertNotNull("Category is null", clue.getCategory());
+                assertNotNull("Clue's clue is null", clue.getClue());
+                assertNotNull("Response is null", clue.getResponse());
+                assertTrue("Correct index is invalid", clue.getCorrectResponseIndex() >= 0);
+                assertTrue("Category is an empty string", clue.getCategory().length() > 0);
+                assertTrue("Response is an empty string", clue.getResponse().length() > 0);
+                assertTrue("Clue's clue is an empty string", clue.getClue().length() > 0);
                 for (String resp : clue.getSelectableAnswers()) {
-                    assertNotNull(resp);
-                    assertTrue(resp.length() > 0);
+                    assertNotNull("Selectable response is null", resp);
+                    assertTrue("Selectable response is an empty string", resp.length() > 0);
                 }
             }
         }
     }
 
+    /**
+     * Called when the board is ready to be used
+     *
+     * @param board a reference to the model GameBoard
+     */
     public void display(GameBoard board) {
         myBoard = board;
         myReadyFlag = true;
